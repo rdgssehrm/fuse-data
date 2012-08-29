22c4528cabbcec33bff93714792ea05d938ead2c
Hugo Mills <h.r.mills@reading.ac.uk>
2012-08-29 14:47:13 +0100

    Fix handling of server exception reporting
    format_exc() returns a single string, which we convery to bytes. While
    a byte sequence is indeed an iterable, and thus can be used successfully
    as a WSGI middleware output, it's far from efficient, and it's a better
    idea to return the bytes as an element of a list.
    
    Signed-off-by: Hugo Mills <h.r.mills@reading.ac.uk>

--- fuse/muddleware.py
+++ fuse/muddleware.py
@@ -70,7 +70,7 @@ class ExceptionHandler(object):
 			for l in traceback.format_exc().split("\n"):
 				log.error("Exception: %s", l)
 
-			return traceback.format_exc().encode("utf8")
+			return [traceback.format_exc().encode("utf8")]
 
 class ParameterisedMiddleware(object):
 	"""Class decorator which allows us to write middlewares which bind
